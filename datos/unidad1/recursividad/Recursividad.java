a42a68b5bfb8f548ea4487cc2779315e3a111aa5
EmmanuelArre <1225100996.jeae@gmail.com>
2026-09-24 15:48:37 -0600

    Ahora muestra Hola Nombre

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -2,18 +2,19 @@ package datos.unidad1.recursividad;
 
 public class Recursividad {
 
-    public static void saludo(int total) {
+    public static void saludo(int total, String nombre) {
 
         if (total <= 0)
             return;
         else
-            System.out.println("Hola");
+            System.out.println("Hola " + nombre);
 
-        saludo(total - 1);
+        saludo(total - 1, nombre);
     }
 
     public static void main(String[] a) {
 
-        saludo(10);
+        saludo(10, "Emmanuel");
+
     }
 }
